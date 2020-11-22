a0d5a2bd7151731b1026bc881f64b24c2d8be6cc
To Dung Nguyen <to-dung.nguyen@u-psud.fr>
2020-11-22 17:08:13 +0100

    refactor code

--- src/App.java
+++ src/App.java
@@ -6,6 +6,10 @@ public class App {
     private static int n;
     // capacité maximum
     private static float B;
+    // liste de poid
+    private static float[] weights;
+    // liste d'utilité
+    private static float[] utilities;
     // liste d'objet
     private static Item[] items;
     // solution actuelle (après le calcul)
@@ -176,6 +180,10 @@ public class App {
         // Vous pouvez les tester ou faire un nouveau fonction de test (qui contient les valeurs de n, B et les objets) comme au-dessous
         // Puis l'appeler ici pour tester
         test1();
+        items = new Item[n];
+        for(int i = 0; i < n; i++) {
+            items[i] = new Item(i, utilities[i], weights[i]);
+        }
         Solution solution = solve(items);
 
         System.out.println();
@@ -186,67 +194,55 @@ public class App {
     public static void test1() {
         n = 4;
         B = 17;
-        items = new Item[n];
-        items[0] = new Item(0, 8, 3);
-        items[1] = new Item(1, 18, 7);
-        items[2] = new Item(2, 20, 9);
-        items[3] = new Item(3, 11, 6);
+        weights = new float[] {3, 7, 9, 6};
+        utilities = new float[] {8, 18, 20, 11};
     }
 
     // 235 (1, 0, 1, 1, 0)
     public static void test2() {
         n = 5;
         B = 10;
-        items = new Item[n];
-        items[0] = new Item(0, 40, 2);
-        items[1] = new Item(1, 50, (float) 3.14);
-        items[2] = new Item(2, 100, (float) 1.98);
-        items[3] = new Item(3, 95, 5);
-        items[4] = new Item(4, 30, 3);
+        weights = new float[] {2, (float) 3.14, (float) 1.98, 5, 3};
+        utilities = new float[] {40, 50, 100, 95, 30};
     }
 
     // 220 (0, 1, 1)
     public static void test3() {
         n = 3;
         B = 50;
-        items = new Item[n];
-        items[0] = new Item(0, 60, 10);
-        items[1] = new Item(1, 100, 20);
-        items[2] = new Item(2, 120, 30);
+        weights = new float[] {10, 20, 30};
+        utilities = new float[] {60, 100, 120};
     }
 
     // 200 (1, 1, 0, 1)
     public static void test4() {
         n = 4;
         B = 60;
-        items = new Item[n];
-        items[0] = new Item(0, 40, 20);
-        items[1] = new Item(1, 100, 10);
-        items[2] = new Item(2, 50, 40);
-        items[3] = new Item(3, 60, 30);
+        weights = new float[] {20, 10, 40, 30};
+        utilities = new float[] {40, 100, 50, 60};
     }
 
     // 50 (0, 1, 1, 1, 0, 1)
     public static void test5() {
         n = 6;
         B = 60;
-        items = new Item[n];
-        items[0] = new Item(0, 2, 10);
-        items[1] = new Item(1, 10, 4);
-        items[2] = new Item(2, 12, 20);
-        items[3] = new Item(3, 18, 24);
-        items[4] = new Item(4, 9, 18);
-        items[5] = new Item(5, 10, 5);
+        weights = new float[] {10, 4, 20, 24, 18, 5};
+        utilities = new float[] {2, 10, 12, 18, 9, 10};
     }
 
     // 56 (1, 1, 0, 1)
     public static void test6() {
         n = 4;
         B = 21;
-        items = new Item[n];
-        items[0] = new Item(0, 18, 6);
-        items[1] = new Item(1, 20, 3);
-        items[2] = new Item(2, 14, 5);
-        items[3] = new Item(3, 18, 9);
+        weights = new float[] {6, 3, 5, 9};
+        utilities = new float[] {18, 20, 14, 18};
+    }
+
+    // 49 (0, 1, 1, 1, 0)
+    public static void test7() {
+        n = 5;
+        B = 26;
+        weights = new float[] {12, 7, 11, 8, 9};
+        utilities = new float[] {24, 13, 23, 15, 16};
     }
 }
